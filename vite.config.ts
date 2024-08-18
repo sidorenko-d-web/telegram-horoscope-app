54b9a1ceb7f7788ec5f2f9565bcd5a77c4f39d4f
sidorenko.d.web <sidorenko-d-web@gmail.com>
2024-08-18 17:05:35 +0500

    prepare for deploing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  //@ts-ignore
-  server: { https: true, host: 'app.test.tg '}, // Not needed for Vite 5+
-  plugins: [mkcert(), react()],
+  plugins: [, react()],
+  base: '/telegram-horoscope-app/'
 });
 
